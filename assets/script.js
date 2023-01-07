30c82e6a6f91d5d09cba27effb9d9aba18613efa
AMess33 <amess3309@gmail.com>
2023-01-06 22:35:06 -0600

    update html file psuedo code js file

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+// create a start button to start the game
+// when start button is hit the first question and a timer appear
+// the timer starts counting down upon start
+// the question is followed by 4 selectable answers
+// when the player selects a correct answer they are given points
+// when the player selects a wrong answer the timer loses 10 seconds
+// repeate the process for 4 seperate questions
+// add the points from each correct answer
+// present the score at the end of the quiz
+// if the timer runs out before all questions are answered the game ends
+// allow player to enter their initials next to their score
+// create a leaderboard with previous high scores
+// display leaderboard at end of game
+// allow player to play again
